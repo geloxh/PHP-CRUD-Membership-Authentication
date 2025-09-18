e49338f1efbd223060d24f896c7448d4fc20ce7e
Angelo <ace.blazado7@gmail.com>
2025-09-18 17:12:15 +0800

    Commit changes in register.php index.php

--- register.php
+++ register.php
@@ -1,1 +1,26 @@
-ECHO is on.
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Register Page - PHP CRUD Membership Authentication</title>
+        <link rel="stylesheet" href="">
+    </head>
+
+    <body>
+        <h2>Create Account</h2>
+        <a href="index.php">Return to Home page<br/><br/>
+        
+        <form action="register.php" method="POST">
+            Username: <input type="text"
+            name="username" required="required" /><br/>
+            Password: <input type="password"
+            name="password" required="required" /><br/>
+            Re-Enter Password: <input type="re-en-password"
+            name="re-en-password" required="required" /><br/>
+            <input type="submit" value="Register" />
+        </form>
+    </body>
+</html>
+
+
